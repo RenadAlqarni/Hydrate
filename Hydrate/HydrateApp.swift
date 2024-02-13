a844a606f58321a6d4a1f67efdb8c7651506c762
RenadAlqarni <renadsalqarni@gmail.com>
2024-02-13 16:23:16 +0300

    Initial Commit

---
+++ Hydrate/HydrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrateApp.swift
+//  Hydrate
+//
+//  Created by Renad Alqarni on 13/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct HydrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
